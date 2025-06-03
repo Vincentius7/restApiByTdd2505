88da0aa53cbf1b8a80699bd6c819fa440d2f022e
sik2 <bickwang123@gmail.com>
2025-06-04 08:52:31 +0900

    fix: test

---
+++ src/test/java/com/ll/restByTdd/RestByTddApplicationTests.java
@@ -0,0 +1,11 @@
+package com.ll.restByTdd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class RestByTddApplicationTests {
+    @Test
+    void contextLoads() {
+    }
+}
